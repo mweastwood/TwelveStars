63eeb95df942a1b170d7dfa4be20be27c82a1b62
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-07-02 17:48:29 -0700

    chore: revert TolerantComparator and update golden images to match remote CI rendering

--- app/test/flutter_test_config.dart
+++ app/test/flutter_test_config.dart
@@ -1,44 +1,8 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
-
-class TolerantComparator extends LocalFileComparator {
-  final double precisionTolerance;
-
-  TolerantComparator(super.testFile, this.precisionTolerance);
-
-  @override
-  Future<bool> compare(Uint8List imageBytes, Uri golden) async {
-    final ComparisonResult result = await GoldenFileComparator.compareLists(
-      imageBytes,
-      await getGoldenBytes(golden),
-    );
-
-    if (result.passed || result.diffPercent <= precisionTolerance) {
-      result.dispose();
-      return true;
-    }
-
-    final error = await generateFailureOutput(result, golden, basedir);
-    result.dispose();
-    throw FlutterError(error);
-  }
-}
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   await loadAppFonts();
-
-  final oldComparator = goldenFileComparator;
-  if (oldComparator is LocalFileComparator) {
-    // resolve('dummy.dart') is needed because LocalFileComparator constructor resolves the parent
-    // directory if the passed URI is already a directory.
-    goldenFileComparator = TolerantComparator(
-      oldComparator.basedir.resolve('dummy.dart'),
-      0.03, // 3% tolerance
-    );
-  }
-
   return GoldenToolkit.runWithConfiguration(() async {
     await testMain();
   }, config: GoldenToolkitConfiguration());
