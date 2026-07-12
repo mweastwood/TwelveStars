6fd3f810d1fbb37885302cef7c6e7837bf901007
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-07-12 15:09:02 -0700

    refactor: Use unified getAiService from plugin

--- app/lib/logic/ai_service_helper.dart
+++ app/lib/logic/ai_service_helper.dart
@@ -1,19 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:local_agent/local_agent.dart';
 
 class LocalAgentHelper {
   static AiService? _instance;
 
   static AiService get instance {
-    if (_instance == null) {
-      if (kIsWeb) {
-        _instance = getWebAiService();
-      } else if (defaultTargetPlatform == TargetPlatform.android) {
-        _instance = MethodChannelAiService();
-      } else {
-        _instance = MockAiService();
-      }
-    }
+    _instance ??= getAiService();
     return _instance!;
   }
 }
