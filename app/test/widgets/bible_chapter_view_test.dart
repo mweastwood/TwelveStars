fddf7a7828858e0533560adfbe35b4a734174947
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-09-19 03:59:43 +0000

    Revert "fix(bible): calibrate chapter citations chip baseline alignment to 9.0px"
    This reverts commit aaff649c61eecce853676310f97b45fe6d7e38e0.

--- app/test/widgets/bible_chapter_view_test.dart
+++ app/test/widgets/bible_chapter_view_test.dart
@@ -280,20 +280,11 @@ void main() {
           findsOneWidget,
         );
 
-        // Verify the bottom of the chip is aligned with the baseline of the chapter title text
+        // Verify the bottom of the chip is positioned with the 6.0px descender offset
+        // to align with the CinzelDecorative baseline of the chapter title text
         final titleRect = tester.getRect(find.text('Genesis 1'));
         final chipRect = tester.getRect(chipFinder);
-        final titleWidget = tester.widget<Text>(find.text('Genesis 1'));
-        final textPainter = TextPainter(
-          text: TextSpan(text: titleWidget.data, style: titleWidget.style),
-          textDirection: TextDirection.ltr,
-        )..layout();
-        final titleBaseline =
-            titleRect.top +
-            textPainter.computeDistanceToActualBaseline(
-              TextBaseline.alphabetic,
-            );
-        expect(chipRect.bottom, closeTo(titleBaseline, 0.5));
+        expect(chipRect.bottom, closeTo(titleRect.bottom - 6.0, 0.5));
         expect(chipRect.left, greaterThan(titleRect.right));
 
         // Verify tapping the chip invokes showReverseCitationsModal
